dd1dbc19fef06bf0b09ceee43cc74da12a819230
Khushali Ganatra <ganatra.khushali@gmail.com>
2019-11-12 17:13:47 +0530

    Not needed
    Not needed

--- footer.php
+++ footer.php
@@ -4,11 +4,10 @@
           <div class="col-md-12">
             <p>
   
-            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://lms-kjsce.somaiya.edu/" target="_blank" class="text-primary">Sem5-Studs</a>
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://kjsce.somaiya.edu/" target="_blank" class="text-primary">Sem5-Students</a>
 
             </p>
           </div>
-          
         </div>
       </div>
     </footer>
